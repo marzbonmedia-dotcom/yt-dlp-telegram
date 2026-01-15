a8333a36d94fa5f044b2ead36d439da999f692f9
ssebastianoo <sebastianogirotto05@gmail.com>
2026-01-15 14:56:39 +0100

    use regex

--- main.py
+++ main.py
@@ -29,7 +29,10 @@ def test(message):
     bot.reply_to(
         message, "*Send me a video link* and I'll download it for you, works with *YouTube*, *Twitter*, *TikTok*, *Reddit* and more.\n\n_Powered by_ [yt-dlp](https://github.com/yt-dlp/yt-dlp/)", parse_mode="MARKDOWN", disable_web_page_preview=True)
 
-def download_video(message, url, audio=False, format_id="mp4"):
+def download_video(message, content, audio=False, format_id="mp4"):
+
+    url = re.search(r'https?://\S+', content).group(0) if re.search(r'https?://\S+', content) else content
+
     url_info = urlparse(url)
     if url_info.scheme:
         if url_info.netloc in ['www.youtube.com', 'youtu.be', 'youtube.com', 'youtu.be']:
