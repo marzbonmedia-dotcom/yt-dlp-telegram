6bb14e8298a641ed15fa3fae9940c74bb13aadce
marzbonmedia-dotcom <marzbonmedia@gmail.com>
2026-01-20 02:30:48 +0300

    Update Telegram token in example.config.py

--- example.config.py
+++ example.config.py
@@ -1,4 +1,4 @@
-token = "8558839549:AAF4CSC7LHuD0_G7MgbXwNOdDPoiYcDbPhk"  # telegram token
+token = "8241193064:AAGgX0HUFKs4YCPR5fkVLlK6ecSYlHwtLCc"  # telegram token
 logs = 2390049  # logs channel id, if none set to None
 max_filesize = 50000000  # bytes
 output_folder="/tmp/yt-dlp-telegram"
